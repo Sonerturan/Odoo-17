541eeab88552103d4242b8799d4953f79df4c474
Soner Turan <sonerturan609@gmail.com>
2025-08-12 09:03:50 +0300

    feat(base): Creating models and views + Tracking + Sequence + No update Attribute + Rec Name + Statubar + Buttons + Conditional Invisible +Many2many Field + Icon For Menu and App + Widget Handle + Editable Attribute

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Hospital Managament System',
+    'version' : '1.2',
+    'summary': 'Hospital',
+    'sequence': -1,
+    "author": "Soner Turan ",
+    'description': """
+Hospital Managament System Development
+    """,
+    'category': 'Human Resources',
+    'website': 'https://www.odoo.com/app/invoicing',
+    'depends': [
+        'mail'
+    ],
+    'data': [
+        "security/ir.model.access.csv",
+        "data/sequence.xml",
+        "views/patient_views.xml",
+        "views/patient_tag_views.xml",
+        "views/patient_readonly_views.xml",
+        "views/appointment_views.xml",
+        "views/menu.xml"
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
