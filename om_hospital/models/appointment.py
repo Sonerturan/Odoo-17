f9cd4dd4d0b51682c222a4c71d707bd24f9b788f
Soner Turan <sonerturan609@gmail.com>
2025-08-14 23:10:19 +0300

    feat(appointment): Related Field

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -17,7 +17,7 @@ class HospitalAppointment(models.Model):
     )
     appointment_line_ids = fields.One2many('hospital.appointment.line', 'appointment_id', string="Lines")
     total_qty = fields.Float(compute='_compute_total_quantity', string="Total Quantity", store=True)
-
+    date_of_birth = fields.Date(string="DOB", related='patient_id.date_of_birth', store=True)
 
     @api.model_create_multi
     def create(self, vals_list):
