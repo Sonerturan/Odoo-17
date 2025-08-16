f98de20c044838573f08ea772c6a6bc60547f6a3
Soner Turan <sonerturan609@gmail.com>
2025-08-16 12:18:38 +0300

    feat(appointment): Inheriting Delete Method + Unlink Method + Validation Error and User Error + Warning Message

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -1,4 +1,8 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class HospitalAppointment(models.Model):
     _name = 'hospital.appointment'
@@ -11,7 +15,7 @@ class HospitalAppointment(models.Model):
     # ondelete='restrict' --> patient deki bağlı olduğu ilgili kaydın silinmesine izin vermez hata verir (çünkü bu kayıt ona bağlı)
     # ondelete='cascade' --> patient deki kayıt silindiğinde appointment daki ilgili kayıtları siler. Tam tersini de yapabilir.
     # ondelete='set null' --> patient deki kayıt silindiğinde appointment daki ilgili kayıtları null yapar. (required=False olmalı çünkü null atayacak)
-    patient_id = fields.Many2one('hospital.patient', string="Patient", required=False, ondelete='set null')
+    patient_id = fields.Many2one('hospital.patient', string="Patient", required=False, ondelete='restrict')
     date_appointment = fields.Date(string="Date")
     note = fields.Text(string="Note")
     state = fields.Selection(
@@ -22,12 +26,24 @@ class HospitalAppointment(models.Model):
     total_qty = fields.Float(compute='_compute_total_quantity', string="Total Quantity", store=True)
     date_of_birth = fields.Date(string="DOB", related='patient_id.date_of_birth', store=True)
 
+    # Form'daki Save (Create) Butonunu Inherit Alma
+    # buradaki vals_list form' a create anında gönderilen verileri içerir.
     @api.model_create_multi
     def create(self, vals_list):
+        _logger.info("-logger Appointment created: %s", vals_list)
         for vals in vals_list:
             if not vals.get('reference') or vals['reference'] == 'New':
                 vals['reference'] = self.env['ir.sequence'].next_by_code('hospital.appointment')
         return super().create(vals_list)
+
+    # Silme işlemini inherit alma
+    # def unlink: Model Bazlı silme kuralı / @api.ondelete de kullanılabilir: ilişkisel kayıt kontrolü (One2many, Many2one)
+    def unlink(self):
+        _logger.info("-logger Appointment deleted. IDs: %s", self.ids)
+        for rec in self:
+            if rec.state != 'draft':  # Silme işlemini sadece draft olanlarda gerçekleştirir
+                raise ValidationError(_("You can delete appoinment only in 'Draft' status !"))
+        return super().unlink()  # Yeni Odoo sürümüyle gelen kısaltma özelliği -> eski hali: return super(HospitalPatient, self).unlink()
 
     @api.depends('appointment_line_ids', 'appointment_line_ids.qty')
     def _compute_total_quantity(self):
--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -1,4 +1,5 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
 
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
@@ -15,3 +16,13 @@ class HospitalPatient(models.Model):
     )
     tag_ids = fields.Many2many('patient.tag', 'patient_tag_rel', 'patient_id', 'tag_id', string='Tags')
     #tag_ids = fields.Many2many('patient.tag', string='Tags')
+
+    # Silme işlemini inherit alma
+    # def unlink: Model Bazlı silme kuralı / @api.ondelete de kullanılabilir: ilişkisel kayıt kontrolü (One2many, Many2one)
+    def unlink(self):
+        for rec in self:
+            domain = [('patient_id', '=', rec.id)]
+            appointments = self.env['hospital.appointment'].search(domain)
+            if appointments:
+                raise ValidationError(_("You cannot delete the patient now.\nAppointment existing for this patient: %s"% rec.name))
+        return super().unlink() # Yeni Odoo sürümüyle gelen kısaltma özelliği -> eski hali: return super(HospitalPatient, self).unlink()
